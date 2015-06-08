a3cb0e112d6eb3825997d570bb85b4bad75c6e05
control <control@yahoo-inc.com>
2015-06-08 09:40:58 +0800

    Initial Commit

---
+++ test4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test4
+//
+//  Created by Curtis Kang on 6/8/15.
+//  Copyright (c) 2015 Curtis Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test4
+//
+//  Created by Curtis Kang on 6/8/15.
+//  Copyright (c) 2015 Curtis Kang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
